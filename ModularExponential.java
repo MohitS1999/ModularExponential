fc9ca78440dcffaae90aed19de33eb33a942830b
Mohit singh <singhmohit9718@gmail.com>
2020-05-03 15:03:52 +0530

    Exponential

---
+++ ModularExponential.java
@@ -0,0 +1,40 @@
+//it is also Called Binary Exponential
+//find the a^b in O(log n) Complexity
+import java.util.*;
+public class ModularExponential{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+			long a=sc.nextInt();
+			long b=sc.nextInt();
+			System.out.println("Iterative Funtion  :-  "+pow(a,b));
+			System.out.println("Reecursive Function  :-  "+binPow(a,b));
+			//Inbuilt function
+			//System.out.println("a^b11  :-  "+(Math.pow(a,b))%1000000007);
+		}
+	}
+	//using Iterative Function
+	public static long pow(long a,long b){
+		long res=1;
+		while(b>0){
+			//if the b is odd
+			if ((b&1)==1)
+				res=(res*a);
+			//b/=2
+			b=b>>1;
+			a=(a*a);
+		}
+		return res;
+	}
+	//Using Recursion
+	public static long binPow(long a,long b){
+		if (b==0)
+			return 1;
+		long res=binPow(a,b>>1);
+		if (b%2!=0)
+			return res*res*a;
+		else
+			return res*res;
+	}
+}
